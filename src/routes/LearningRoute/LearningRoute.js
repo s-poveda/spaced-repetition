d14723a331773174113451a660f9858df3256404
s-poveda <spoveda545@gmail.com>
2020-12-26 00:40:20 -0500

    learn route first pass

--- src/routes/LearningRoute/LearningRoute.js
+++ src/routes/LearningRoute/LearningRoute.js
@@ -17,8 +17,23 @@ class LearningRoute extends Component {
 			<section>
 				<div>
 				<h2>Translate the word:</h2>
-				<span>{this.state.head.original}</span>
+				<span>{this.state.head.nextWord}</span>
 				</div>
+        <form>
+          <label htmlFor='learn-guess-input'>{"What's the translation for this word?"}</label>
+          <input id='learn-guess-input' type='text' required />
+          <button type='submit'>Submit your answer</button>
+        </form>
+        <div>
+          // REVIEW: this may need fixing with state properties. Unable to test code ATM
+          // Just going off of what tests have
+          <p>
+            You have answered this word correctly ${this.state.head.correct_count} times.
+          </p>
+          <p>
+            You have answered this word incorrectly ${this.state.head.incorrect_count} times.
+          </p>
+        </div>
 			</section>
 		);
   }
